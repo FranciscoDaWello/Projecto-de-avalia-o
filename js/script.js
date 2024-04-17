61637ff6207c8b9c230822244b5ab0e9d81ec020
FranciscoDaWello <146932488+FranciscoDaWello@users.noreply.github.com>
2024-04-18 01:14:40 +0200

    Projecto de avaliação Francisco

---
+++ js/script.js
@@ -0,0 +1,281 @@
+/*Criação do somatorio das alinhas de maneira estática
+efectuando o reuso do código para todas elas apenas alterando a sua
+numeração... Optei por esse caminho para poder terminar a tempo o projecto e de acordo a minha percepção
+no momento da execução */
+ function validateAndUpdate() {
+        var funcionaisInput2 = document.getElementById('funcionais2');
+        var nfuncionaisInput2 = document.getElementById('nfuncionais2');
+        var totalInput2 = document.getElementById('total2');
+
+        var primeirovalor2 = parseInt(funcionaisInput2.value);
+        var segundovalor2 = parseInt(nfuncionaisInput2.value);
+        
+        if (primeirovalor2 <= 0 || segundovalor2 <= 0) {
+            totalInput2.value = 0;
+            totalInput2.disabled = true;
+        } else {
+             var total2 = primeirovalor2 + segundovalor2;
+            totalInput2.value = total2;
+            totalInput2.disabled = true;
+        }
+/*---------------------------------*/
+        var funcionaisInput3 = document.getElementById('funcionais3');
+        var nfuncionaisInput3 = document.getElementById('nfuncionais3');
+        var totalInput3 = document.getElementById('total3');
+
+        var primeirovalor3 = parseInt(funcionaisInput3.value);
+        var segundovalor3 = parseInt(nfuncionaisInput3.value);
+        
+        if (primeirovalor3 <= 0 || segundovalor3 <= 0) {
+            totalInput3.value = 0;
+            totalInput3.disabled = true;
+        } else {
+             var total3 = primeirovalor3 + segundovalor3;
+            totalInput3.value = total3;
+            totalInput3.disabled = true;
+        }
+/*---------------------------------*/
+        var funcionaisInput4 = document.getElementById('funcionais4');
+        var nfuncionaisInput4 = document.getElementById('nfuncionais4');
+        var totalInput4 = document.getElementById('total4');
+
+        var primeirovalor4 = parseInt(funcionaisInput4.value);
+        var segundovalor4 = parseInt(nfuncionaisInput4.value);
+        
+        if (primeirovalor4 <= 0 || segundovalor4 <= 0) {
+            totalInput4.value = 0;
+            totalInput4.disabled = true;
+        } else {
+             var total4 = primeirovalor4 + segundovalor4;
+            totalInput4.value = total4;
+            totalInput4.disabled = true;
+        }
+/*---------------------------------*/
+        var funcionaisInput5 = document.getElementById('funcionais5');
+        var nfuncionaisInput5 = document.getElementById('nfuncionais5');
+        var totalInput5 = document.getElementById('total5');
+
+        var primeirovalor5 = parseInt(funcionaisInput5.value);
+        var segundovalor5 = parseInt(nfuncionaisInput5.value);
+        
+        if (primeirovalor5 <= 0 || segundovalor5 <= 0) {
+            totalInput5.value = 0;
+            totalInput5.disabled = true;
+        } else {
+             var total5 = primeirovalor5 + segundovalor5;
+            totalInput5.value = total5;
+            totalInput5.disabled = true;
+        }
+/*---------------------------------*/
+        var funcionaisInput6 = document.getElementById('funcionais6');
+        var nfuncionaisInput6 = document.getElementById('nfuncionais6');
+        var totalInput6 = document.getElementById('total6');
+
+        var primeirovalor6 = parseInt(funcionaisInput6.value);
+        var segundovalor6 = parseInt(nfuncionaisInput6.value);
+        
+        if (primeirovalor6 <= 0 || segundovalor6 <= 0) {
+            totalInput6.value = 0;
+            totalInput6.disabled = true;
+        } else {
+             var total6 = primeirovalor6 + segundovalor6;
+            totalInput6.value = total6;
+            totalInput6.disabled = true;
+        }
+/*---------------------------------*/
+        var funcionaisInput7 = document.getElementById('funcionais7');
+        var nfuncionaisInput7 = document.getElementById('nfuncionais7');
+        var totalInput7 = document.getElementById('total7');
+
+        var primeirovalor7 = parseInt(funcionaisInput7.value);
+        var segundovalor7 = parseInt(nfuncionaisInput7.value);
+        
+        if (primeirovalor7 <= 0 || segundovalor7 <= 0) {
+            totalInput7.value = 0;
+            totalInput7.disabled = true;
+        } else {
+             var total7 = primeirovalor7 + segundovalor7;
+            totalInput7.value = total7;
+            totalInput7.disabled = true;
+        }
+/*---------------------------------*/
+        var funcionaisInput8 = document.getElementById('funcionais8');
+        var nfuncionaisInput8 = document.getElementById('nfuncionais8');
+        var totalInput8 = document.getElementById('total8');
+
+        var primeirovalor8 = parseInt(funcionaisInput8.value);
+        var segundovalor8 = parseInt(nfuncionaisInput8.value);
+        
+        if (primeirovalor8 <= 0 || segundovalor8 <= 0) {
+            totalInput8.value = 0;
+            totalInput8.disabled = true;
+        } else {
+             var total8 = primeirovalor8 + segundovalor8;
+            totalInput8.value = total8;
+            totalInput8.disabled = true;
+        }
+/*---------------------------------*/
+        var funcionaisInput9 = document.getElementById('funcionais9');
+        var nfuncionaisInput9 = document.getElementById('nfuncionais9');
+        var totalInput9 = document.getElementById('total9');
+
+        var primeirovalor9 = parseInt(funcionaisInput9.value);
+        var segundovalor9 = parseInt(nfuncionaisInput9.value);
+        
+        if (primeirovalor9 <= 0 || segundovalor9 <= 0) {
+            totalInput9.value = 0;
+            totalInput9.disabled = true;
+        } else {
+             var total9 = primeirovalor9 + segundovalor9;
+            totalInput9.value = total9;
+            totalInput9.disabled = true;
+        }
+/*---------------------------------*/
+        var funcionaisInput10 = document.getElementById('funcionais10');
+        var nfuncionaisInput10 = document.getElementById('nfuncionais10');
+        var totalInput10 = document.getElementById('total10');
+
+        var primeirovalor10 = parseInt(funcionaisInput10.value);
+        var segundovalor10 = parseInt(nfuncionaisInput10.value);
+        
+        if (primeirovalor10 <= 0 || segundovalor10 <= 0) {
+            totalInput10.value = 0;
+            totalInput10.disabled = true;
+        } else {
+             var total10 = primeirovalor10 + segundovalor10;
+            totalInput10.value = total10;
+            totalInput10.disabled = true;
+        }
+/*---------------------------------*/
+        var funcionaisInput11 = document.getElementById('funcionais11');
+        var nfuncionaisInput11 = document.getElementById('nfuncionais11');
+        var totalInput11 = document.getElementById('total11');
+
+        var primeirovalor11 = parseInt(funcionaisInput11.value);
+        var segundovalor11 = parseInt(nfuncionaisInput11.value);
+        
+        if (primeirovalor11 <= 0 || segundovalor11 <= 0) {
+            totalInput11.value = 0;
+            totalInput11.disabled = true;
+        } else {
+             var total11 = primeirovalor11 + segundovalor11;
+            totalInput11.value = total11;
+            totalInput11.disabled = true;
+        }
+/*---------------------------------*/
+        var funcionaisInput12 = document.getElementById('funcionais12');
+        var nfuncionaisInput12 = document.getElementById('nfuncionais12');
+        var totalInput12 = document.getElementById('total12');
+
+        var primeirovalor12 = parseInt(funcionaisInput12.value);
+        var segundovalor12 = parseInt(nfuncionaisInput12.value);
+        
+        if (primeirovalor12 <= 0 || segundovalor12 <= 0) {
+            totalInput12.value = 0;
+            totalInput12.disabled = true;
+        } else {
+             var total12 = primeirovalor12 + segundovalor12;
+            totalInput12.value = total12;
+            totalInput12.disabled = true;
+        }
+/*---------------------------------*/
+        var funcionaisInput13 = document.getElementById('funcionais13');
+        var nfuncionaisInput13 = document.getElementById('nfuncionais13');
+        var totalInput13 = document.getElementById('total13');
+
+        var primeirovalor13 = parseInt(funcionaisInput13.value);
+        var segundovalor13 = parseInt(nfuncionaisInput13.value);
+        
+        if (primeirovalor13 <= 0 || segundovalor13 <= 0) {
+            totalInput13.value = 0;
+            totalInput13.disabled = true;
+        } else {
+             var total13 = primeirovalor13 + segundovalor13;
+            totalInput13.value = total13;
+            totalInput13.disabled = true;
+        }
+/*---------------------------------*/
+        var funcionaisInput14 = document.getElementById('funcionais14');
+        var nfuncionaisInput14 = document.getElementById('nfuncionais14');
+        var totalInput14 = document.getElementById('total14');
+
+        var primeirovalor14 = parseInt(funcionaisInput14.value);
+        var segundovalor14 = parseInt(nfuncionaisInput14.value);
+        
+        if (primeirovalor14 <= 0 || segundovalor14 <= 0) {
+            totalInput14.value = 0;
+            totalInput14.disabled = true;
+        } else {
+             var total14 = primeirovalor14 + segundovalor14;
+            totalInput14.value = total14;
+            totalInput14.disabled = true;
+        }
+/*---------------------------------*/
+        var funcionaisInput15 = document.getElementById('funcionais15');
+        var nfuncionaisInput15 = document.getElementById('nfuncionais15');
+        var totalInput15 = document.getElementById('total15');
+
+        var primeirovalor15= parseInt(funcionaisInput15.value);
+        var segundovalor15 = parseInt(nfuncionaisInput15.value);
+        
+        if (primeirovalor15 <= 0 || segundovalor15 <= 0) {
+            totalInput15.value = 0;
+            totalInput15.disabled = true;
+        } else {
+             var total15 = primeirovalor15 + segundovalor15;
+            totalInput15.value = total15;
+            totalInput15.disabled = true;
+        }
+/*---------------------------------*/
+        var funcionaisInput16 = document.getElementById('funcionais16');
+        var nfuncionaisInput16 = document.getElementById('nfuncionais16');
+        var totalInput16 = document.getElementById('total16');
+
+        var primeirovalor16 = parseInt(funcionaisInput16.value);
+        var segundovalor16 = parseInt(nfuncionaisInput16.value);
+        
+        if (primeirovalor16 <= 0 || segundovalor16 <= 0) {
+            totalInput16.value = 0;
+            totalInput16.disabled = true;
+        } else {
+             var total16 = primeirovalor16 + segundovalor16;
+            totalInput16.value = total16;
+            totalInput16.disabled = true;
+        }
+/*---------------------------------*/
+        var funcionaisInput17 = document.getElementById('funcionais17');
+        var nfuncionaisInput17 = document.getElementById('nfuncionais17');
+        var totalInput17 = document.getElementById('total17');
+
+        var primeirovalor17 = parseInt(funcionaisInput17.value);
+        var segundovalor17 = parseInt(nfuncionaisInput17.value);
+        
+        if (primeirovalor17 <= 0 || segundovalor17 <= 0) {
+            totalInput17.value = 0;
+            totalInput17.disabled = true;
+        } else {
+             var total17 = primeirovalor17 + segundovalor17;
+            totalInput17.value = total17;
+            totalInput17.disabled = true;
+        }
+/*---------------------------------*/
+        var funcionaisInput18 = document.getElementById('funcionais18');
+        var nfuncionaisInput18 = document.getElementById('nfuncionais18');
+        var totalInput18 = document.getElementById('total18');
+
+        var primeirovalor18 = parseInt(funcionaisInput18.value);
+        var segundovalor18 = parseInt(nfuncionaisInput18.value);
+        
+        if (primeirovalor18 <= 0 || segundovalor18 <= 0) {
+            totalInput18.value = 0;
+            totalInput18.disabled = true;
+        } else {
+             var total18 = primeirovalor18 + segundovalor18;
+            totalInput18.value = total18;
+            totalInput18.disabled = true;
+        }
+
+
+
+
+    }
